a4f39fb67b2ee863b335f46280d881b596621157
unknown <polishettianusha@gmail.com>
2023-04-29 21:53:25 +0530

    Submitting the project

---
+++ src/main/java/com/driver/ApproverNotAdminException.java
@@ -0,0 +1,7 @@
+package com.driver;
+
+public class ApproverNotAdminException extends RuntimeException {
+    public ApproverNotAdminException() {
+        super("Approver does not have rights");
+    }
+}
